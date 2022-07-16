1fbcf3e5eada6e739961d96a407233820b51cb47
Akash <akhvnd@gmail.com>
2022-07-16 15:24:42 +0530

    modified:Player rolls the dice

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,12 +3,23 @@ package com.SnakeLadder.java;
 public class SnakeLadder {
 	
 	static int startingPosition = 0;
-	static int endingPosition = 0; 
+	static int endingPosition = 100; 
+	
+    public int playerRolls() {
+    	System.out.println(" Player Rolls the Dice ");
+		int number = (int)((Math.random() * 10 ) % 6 + 1 ) ;
+		System.out.println(" Number came on dice : "+ number);
+		return number;
+		
+	}
 
 	public static void main(String[] args) {
 		
+		SnakeLadder dice = new SnakeLadder();
 		System.out.println("Welcome to Snake and Ladder Game");
 		System.out.println(" Player at Starting Position:"+ startingPosition);
+		System.out.println();
+		dice.playerRolls();
 	}
 
 }
