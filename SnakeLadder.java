f4c22e850127ba2bb54f59ca215d1e82d1ddac08
Akash <akhvnd@gmail.com>
2022-07-16 15:16:17 +0530

    modified:single player at start Position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,11 +1,14 @@
 package com.SnakeLadder.java;
 
 public class SnakeLadder {
+	
+	static int startingPosition = 0;
+	static int endingPosition = 0; 
 
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Snake and Ladder Game");
-
+		System.out.println(" Player at Starting Position:"+ startingPosition);
 	}
 
 }
